34ae9b8483f4250038937814abc0d7b7a8138088
Johnie Hjelm <johnie@hjelm.im>
2024-11-03 17:12:17 +0100

    feat: remove old loading component and refactor page for streamlined metadata generation and enhanced notification display

--- app/[...slugs]/page.tsx
+++ app/[...slugs]/page.tsx
@@ -1,55 +1,51 @@
 import React from 'react';
-import Image from 'next/image';
-import { Icons } from '@/components/icons';
-import { createMetaScraper } from '@/lib/meta';
-import { format } from 'date-fns';
-import { sv } from 'date-fns/locale';
+import type { Metadata } from 'next';
+import { Controls } from '@/components/controls/controls';
+import { NotificationPreview } from './_components/notification-preview';
+import { CONTROLS_SEARCH_PARAMS } from '@/lib/params/controls';
 
-export default async function Page({ params }: { params: { slugs: string[] } }) {
+type Params = { slugs: string[] };
+
+type SearchParams = Record<string, string | string[] | undefined>;
+
+export async function generateMetadata({
+  params,
+  searchParams,
+}: {
+  params: Params;
+  searchParams: SearchParams;
+}) {
+  const { slugs } = params;
+  const slug = slugs.join('/');
+  const theme = CONTROLS_SEARCH_PARAMS.theme.parseServerSide(
+    searchParams.theme
+  );
+
+  const darkMode = CONTROLS_SEARCH_PARAMS.darkMode.parseServerSide(
+    searchParams.darkMode
+  );
+
+  return {
+    title: `${slug} | ajour`,
+    openGraph: {
+      title: `ajour - ${slug}`,
+      images: [`/api/og?article=${slug}&theme=${theme}&darkMode=${darkMode}`],
+    },
+  } satisfies Metadata;
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slugs: string[] }>;
+}) {
   const { slugs } = await params;
-  const res = await fetch(`https://omni.se/${slugs.join('/')}`);
-  const html = await res.text();
-  const scrapeMetaData = createMetaScraper({});
+  const slug = slugs.join('/');
 
-  const meta = await scrapeMetaData(html);
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="p-10 bg-gradient-to-tl from-violet-500 to-fuchsia-500 text-white aspect-square">
-        <div className="max-w-lg bg-zinc-50 dark:bg-zinc-900 text-zinc-800 dark:text-zinc-50 p-4 rounded-lg shadow-xl font-[family-name:var(--font-benton-sans)] m-auto border-t-2 dark:border-zinc-800 border-white">
-          <div className="relative">
-            <Image
-              src={meta.image.url}
-              alt={meta.title}
-              width={meta.image.width}
-              height={meta.image.height}
-              className="rounded"
-            />
-          </div>
-
-          {/* Article Content */}
-          <div className="mt-4">
-            {/* Category */}
-            <span className="text-omni-green text-sm uppercase tracking-wider font-bold">{meta.section}</span>
-
-            {/* Title */}
-            <h1 className="text-2xl font-bold mt-2 mb-4 leading-tight text-balance">{meta.title}</h1>
-
-            {/* Author and Date */}
-            <div className="dark:text-zinc-400 text-zinc-600 text-sm space-y-1">
-              <p>Av {meta.author}</p>
-              <p>Publicerad {format(new Date(meta.publishedAt), 'd MMMM, HH:mm', { locale: sv })}</p>
-            </div>
-
-            {/* Article Text */}
-            <div className="mt-4 space-y-4 font-[family-name:var(--font-publico-text)]">
-              <p>{meta.description}</p>
-            </div>
-            <div className="pt-4 dark:text-zinc-600 text-zinc-400">
-              <Icons.omni className="w-12" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Controls />
+      <NotificationPreview slug={slug} />
     </div>
   );
 }
