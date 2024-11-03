fc28ab006aec3fcab506527310afc5a1d025ede1
Johnie Hjelm <johnie@hjelm.im>
2024-11-03 17:13:21 +0100

    feat: integrate next-intl for theme labels in the theme switcher, enhancing localization support for light, dark, and system themes

--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { useTheme } from 'next-themes';
+import { useTranslations } from 'next-intl';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -14,6 +15,7 @@ import {
 
 export function ThemeSwitcher() {
   const { setTheme } = useTheme();
+  const t = useTranslations('navigation');
 
   return (
     <DropdownMenu>
@@ -21,13 +23,19 @@ export function ThemeSwitcher() {
         <Button variant="outline" size="icon">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">{t('theme')}</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          {t('light')}
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          {t('dark')}
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          {t('system')}
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
