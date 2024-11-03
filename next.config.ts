622c014dcc3a58f24e98bd0268eecf27980ef913
Johnie Hjelm <johnie@hjelm.im>
2024-11-03 17:08:09 +0100

    feat: integrate next-intl plugin and enable experimental MDX support in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   images: {
     domains: ['gfx.omni.se'],
   },
+  experimental: {
+    mdxRs: true,
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
