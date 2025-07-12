7558d77d49d02a9cb3927c46c4ae9d8782fd7d6e
Johnie Hjelm <johnie@hjelm.im>
2025-07-12 15:59:28 +0200

    feat: implement useControls hook to manage query states with CONTROLS_SEARCH_PARAMS

---
+++ lib/params/use-controls.tsx
@@ -0,0 +1,6 @@
+import { useQueryStates } from 'nuqs';
+import { CONTROLS_SEARCH_PARAMS } from './controls';
+
+export function useControls() {
+  return useQueryStates(CONTROLS_SEARCH_PARAMS, { clearOnDefault: true });
+}
